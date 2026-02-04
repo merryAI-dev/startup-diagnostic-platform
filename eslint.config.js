bddbdd6fcff5fd1a377ca76d9ca56d2e17377d16
jylee0926 <jylee0926@mysc.co.kr>
2026-02-04 11:16:25 +0900

    chore: initial setting vite,react,tailwind, lint

---
+++ eslint.config.js
@@ -0,0 +1,58 @@
+import js from "@eslint/js"
+import prettier from "eslint-plugin-prettier"
+import react from "eslint-plugin-react"
+import reactHooks from "eslint-plugin-react-hooks"
+import { defineConfig } from "eslint/config"
+import tseslint from "typescript-eslint"
+
+export default defineConfig([
+    // JS 기본 추천 규칙 (TS만 써도 필요)
+    js.configs.recommended,
+
+    // TypeScript 추천 규칙
+    ...tseslint.configs.recommended,
+
+    {
+        files: ["**/*.{ts,tsx}"],
+
+        languageOptions: {
+            ecmaVersion: "latest",
+            sourceType: "module",
+        },
+
+        plugins: {
+            react,
+            "react-hooks": reactHooks,
+            prettier,
+        },
+
+        settings: {
+            react: {
+                version: "detect",
+            },
+        },
+
+        rules: {
+            /* React */
+            "react/react-in-jsx-scope": "off",
+            "react/prop-types": "off",
+
+            /* Hooks */
+            "react-hooks/rules-of-hooks": "error",
+            "react-hooks/exhaustive-deps": "warn",
+
+            /* TypeScript */
+            "@typescript-eslint/no-unused-vars": [
+                "warn",
+                { argsIgnorePattern: "^_" },
+            ],
+            "@typescript-eslint/explicit-module-boundary-types": "off",
+
+            /* Code quality */
+            "prefer-const": "error",
+
+            /* Prettier */
+            "prettier/prettier": "warn",
+        },
+    },
+])
---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+
+import "./app.css"
+
+export function App() {
+
+  return (
+    <div className="min-h-screen bg-red-500 flex items-center justify-center">
+      <div className="text-white text-4xl font-bold">App</div>
+    </div>
+  )
+}
